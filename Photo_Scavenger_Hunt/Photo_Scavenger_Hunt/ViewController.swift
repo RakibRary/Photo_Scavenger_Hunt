419e65acb8a62204c85af1d11248215e62e68e3c
RakibRary <60999379+RakibRary@users.noreply.github.com>
2023-03-20 23:21:04 -0400

    Add files via upload

---
+++ Photo_Scavenger_Hunt/Photo_Scavenger_Hunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo_Scavenger_Hunt
+//
+//  Created by Rakib Rary on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var Click_Here: UILabel!
+}
+
